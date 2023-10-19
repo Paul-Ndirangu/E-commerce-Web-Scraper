c2987bd150ea95ad6d6807c08d8ee4c20007fc8e
Paul-Ndirangu <paulmwaura254@gmail.com>
2023-10-19 22:27:07 +0300

    .

--- scraper.py
+++ scraper.py
@@ -4,3 +4,10 @@ import pandas as pd
 
 baseurl = "https://www.thewhiskyexchange.com"
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'}
+
+page = requests.get('https://www.thewhiskyexchange.com/c/35/japanese-whisky')
+
+
+soup = BeautifulSoup(page.content,'html.parser')
+
+print(soup)
