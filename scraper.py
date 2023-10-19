5f7d319f30e787500cee90744d420ea983cf4079
Paul-Ndirangu <paulmwaura254@gmail.com>
2023-10-19 22:54:32 +0300

    .

--- scraper.py
+++ scraper.py
@@ -1,17 +1,9 @@
-import requests
 from bs4 import BeautifulSoup
-import pandas as pd
+import requests
+url = "https://www.tutorialspoint.com/index.htm"
+req = requests.get(url)
+soup = BeautifulSoup(req.text, "html.parser")
+print(soup.title)
 
-baseurl = "https://www.thewhiskyexchange.com"
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'}
-
-page = requests.get('https://www.thewhiskyexchange.com/c/35/japanese-whisky')
-
-
-soup = BeautifulSoup(page.content,'html.parser')
-
-results = soup.find_all('div', attrs= {'class': 'product-grid__item'})
-
-for element in results:
-    print(element, end="\n"*2)
-    print()
+for link in soup.find_all('a'):
+    print(link.get('href'))
